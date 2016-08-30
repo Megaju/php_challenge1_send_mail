ec17aa0981837398a45422d7ba9ef154ed3cb8a4
Megaju <julien_malle@outlook.fr>
2016-08-30 14:50:10 +0200

    avant pull request

--- send.php
+++ send.php
@@ -1,7 +1,17 @@
 <?php
-    if (isset($_POST['email']) && isset($_POST['obj'])&& isset($_POST['mess'])) {
-        mail($_POST['email'], $_POST['obj'], $_POST['mess']);
-        echo "Message envoyé avec succès !";
+    /*if (isset($_POST["email"]) && isset($_POST["obj"]) && isset($_POST["mess"])) {
+        mail($_POST["email"], $_POST["obj"], $_POST["mess"]);
+        echo "<p>Message envoyé avec succès !</p>";
+    } else {
+        echo "<p>Le message n'a pas été envoyé.</p>";
+    }*/
+?>
+
+
+<?php
+    if (true) {
+        mail($_POST["email"], $_POST["obj"], $_POST["mess"]);
+        echo "<p>Message envoyé avec succès !</p>";
     } else {
         echo "<p>Le message n'a pas été envoyé.</p>";
     }
